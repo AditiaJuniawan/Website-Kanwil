a80ff60067b6db75bc42c0ad29afd0e49d8ced68
Gunawan Wibisono <gunawan.wibisono1989@gmail.com>
2026-06-07 12:43:06 +0700

    config(panel): change admin path to control

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('control')
             ->login()
             ->brandLogo(asset('images/logokementerian.png'))
             ->brandLogoHeight('3rem')
