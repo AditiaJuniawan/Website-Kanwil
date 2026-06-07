11bbfbc3cf24c0e5f9885c5d5d5ba69104409cb3
Gunawan Wibisono <gunawan.wibisono1989@gmail.com>
2026-06-07 14:57:08 +0700

    Add default leaders to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,42 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Seed default leaders if table is empty
+        \App\Models\Leader::firstOrCreate(
+            ['name' => 'Mumammad Ali Syeh Banna,Bc.I.P.,S.Sos.,M.Si'],
+            [
+                'position' => 'Kepala Kantor Wilayah Ditjenpas Banten',
+                'image' => 'kakanwil.png',
+                'order' => 1,
+            ]
+        );
+
+        \App\Models\Leader::firstOrCreate(
+            ['name' => 'Mumamad Khapi, A.Md.I.P.,S.Sos.,M.M'],
+            [
+                'position' => "Kepala Bagian\nTata Usaha dan Umum",
+                'image' => 'kabagtum.png',
+                'order' => 2,
+            ]
+        );
+
+        \App\Models\Leader::firstOrCreate(
+            ['name' => 'Ahmad Hardi, Bc.I.P.,S.H.,M.M'],
+            [
+                'position' => "Kepala Bidang\nPelayanan dan Pembinaan",
+                'image' => 'kabidPK.png',
+                'order' => 3,
+            ]
+        );
+
+        \App\Models\Leader::firstOrCreate(
+            ['name' => 'Ade Kusmanto, A.Md.IP.,S.H.,M.H'],
+            [
+                'position' => "Kepala Bidang\nPembimbing Kemasyarakatan",
+                'image' => 'kabidPKP.png',
+                'order' => 4,
+            ]
+        );
     }
 }
