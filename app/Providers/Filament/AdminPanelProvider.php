678e2453e5aced133fb552285c299782c65e70aa
Gunawan Wibisono <gunawan.wibisono1989@gmail.com>
2026-06-07 13:59:06 +0700

    Revert admin panel path to 'admin'

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('control')
+            ->path('admin')
             ->login()
             ->brandLogo(asset('images/logokementerian.png'))
             ->brandLogoHeight('3rem')
