c544409b8bdc419e2a20d8feee416eab0f340f07
Gunawan Wibisono <gunawan.wibisono1989@gmail.com>
2026-06-16 22:42:28 +0700

    perf: add PDO connection timeout to sultan database configuration

--- config/database.php
+++ config/database.php
@@ -77,6 +77,9 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+            'options' => [
+                PDO::ATTR_TIMEOUT => 3,
+            ],
         ],
 
 
