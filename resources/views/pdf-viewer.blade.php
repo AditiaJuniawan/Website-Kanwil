43d456ca4753c047d2da84071ac234103eee6db6
Gunawan Wibisono <gunawan.wibisono1989@gmail.com>
2026-09-06 01:29:57 +0700

    fix: make pdf viewer responsive on mobile devices

--- resources/views/pdf-viewer.blade.php
+++ resources/views/pdf-viewer.blade.php
@@ -11,7 +11,12 @@
     body { overflow: hidden !important; margin: 0; padding: 0; }
 </style>
 
-<div style="width: 100vw; height: calc(100vh - 114px); background-color: #f1f5f9;">
-    <embed src="{{ $pdfUrl }}" type="application/pdf" width="100%" height="100%" />
+<div style="width: 100vw; height: calc(100vh - 114px); background-color: #f1f5f9; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+    <object data="{{ $pdfUrl }}" type="application/pdf" width="100%" height="100%">
+        <!-- Fallback untuk perangkat mobile yang tidak mendukung preview PDF bawaan -->
+        <iframe src="https://docs.google.com/gview?url={{ urlencode($pdfUrl) }}&embedded=true" width="100%" height="100%" frameborder="0">
+            <p>Browser Anda tidak mendukung pratinjau PDF. <a href="{{ $pdfUrl }}" target="_blank" style="color: #0369a1; text-decoration: underline;">Unduh PDF</a></p>
+        </iframe>
+    </object>
 </div>
 @endsection
